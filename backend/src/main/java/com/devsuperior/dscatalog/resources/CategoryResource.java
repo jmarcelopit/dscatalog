fb5ee435587229527279e406ecfee1e5214bca46
DESKTOP-A8PBUFL\jose.assis <jose.assis@mprj.mp.br>
2022-01-26 17:50:09 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -2,9 +2,12 @@ package com.devsuperior.dscatalog.resources;
 
 import java.net.URI;
 //import java.util.ArrayList;
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,13 +31,27 @@ public class CategoryResource {
 	@Autowired
 	private CategoryService service;
 	
-	@GetMapping
+	/*@GetMapping
 	public ResponseEntity<List<CategoryDTO>> findAll(){
-		/*List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Livros"));
-		list.add(new Category(2L, "Electronics"));*/
+		//List<Category> list = new ArrayList<>();
+		//list.add(new Category(1L, "Livros"));
+		//list.add(new Category(2L, "Electronics"));
 		
 		List<CategoryDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}*/
+	
+	@GetMapping
+	public ResponseEntity<Page<CategoryDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy			
+			){
+		
+		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction),orderBy ); 
+		
+		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
 	
